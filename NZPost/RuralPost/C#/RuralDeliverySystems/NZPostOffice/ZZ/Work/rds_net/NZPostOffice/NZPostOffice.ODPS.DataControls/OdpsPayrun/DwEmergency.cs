b26e5fd8963d5b8bb762748e4a31bd1b7a600c29
Brittont <brittont@fronde.com>
2008-09-04 02:51:55 +0000

    Vss2Git

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsPayrun/DwEmergency.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsPayrun/DwEmergency.cs
@@ -17,7 +17,17 @@ namespace NZPostOffice.ODPS.DataControls.OdpsPayrun
         public DwEmergency()
         {
             InitializeComponent();
-            InitializeDropdown();
+            //InitializeDropdown();
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                InitializeDropdown();
+            }
+            
+            base.OnHandleCreated(e);
         }
 
         private void InitializeDropdown()
