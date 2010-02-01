c30194f1452bbc61919094a46fcefdae834ac432
Brittont <brittont@fronde.com>
2010-02-01 03:49:30 +0000

    Vss2Git

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DAddressSearchPrimContact.designer.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DAddressSearchPrimContact.designer.cs
@@ -36,77 +36,77 @@ namespace NZPostOffice.RDS.DataControls.Ruraldw
 
 		private void InitializeComponent()
 		{
-			components = new System.ComponentModel.Container();
-			this.Name = "DAddressSearchPrimContact";
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
-			this.SuspendLayout();
-			this.bindingSource.DataSource = typeof(NZPostOffice.RDS.Entity.Ruraldw.AddressSearchPrimContact);
-			//
-			// cust_surname_company
-			//
-			cust_surname_company = new System.Windows.Forms.TextBox();
-			this.cust_surname_company.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.cust_surname_company.Font = new System.Drawing.Font("MS Sans Serif", 8F);
-			this.cust_surname_company.ForeColor = System.Drawing.Color.Black;
-			this.cust_surname_company.Location = new System.Drawing.Point(110, 2);
-			this.cust_surname_company.MaxLength = 45;
-			this.cust_surname_company.Name = "cust_surname_company";
-			this.cust_surname_company.Size = new System.Drawing.Size(230, 13);
-			this.cust_surname_company.TextAlign = HorizontalAlignment.Left;
-			this.cust_surname_company.DataBindings.Add(new System.Windows.Forms.Binding("Text",
-				this.bindingSource, "CustSurnameCompany", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-			this.cust_surname_company.TabIndex = 30;
-			this.Controls.Add(cust_surname_company);
+            this.cust_surname_company = new System.Windows.Forms.TextBox();
+            this.cust_initials = new System.Windows.Forms.TextBox();
+            this.cust_surname_company_t = new System.Windows.Forms.Label();
+            this.cust_initials_t = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bindingSource
+            // 
+            this.bindingSource.DataSource = typeof(NZPostOffice.RDS.Entity.Ruraldw.AddressSearchPrimContact);
+            // 
+            // cust_surname_company
+            // 
+            this.cust_surname_company.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bindingSource, "CustSurnameCompany", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.cust_surname_company.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cust_surname_company.ForeColor = System.Drawing.Color.Black;
+            this.cust_surname_company.Location = new System.Drawing.Point(110, 4);
+            this.cust_surname_company.MaxLength = 45;
+            this.cust_surname_company.Name = "cust_surname_company";
+            this.cust_surname_company.Size = new System.Drawing.Size(230, 20);
+            this.cust_surname_company.TabIndex = 30;
+            // 
+            // cust_initials
+            // 
+            this.cust_initials.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bindingSource, "CustInitials", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.cust_initials.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cust_initials.ForeColor = System.Drawing.Color.Black;
+            this.cust_initials.Location = new System.Drawing.Point(110, 25);
+            this.cust_initials.MaxLength = 30;
+            this.cust_initials.Name = "cust_initials";
+            this.cust_initials.Size = new System.Drawing.Size(230, 20);
+            this.cust_initials.TabIndex = 40;
+            // 
+            // cust_surname_company_t
+            // 
+            this.cust_surname_company_t.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cust_surname_company_t.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cust_surname_company_t.ForeColor = System.Drawing.Color.Black;
+            this.cust_surname_company_t.Location = new System.Drawing.Point(0, 3);
+            this.cust_surname_company_t.Name = "cust_surname_company_t";
+            this.cust_surname_company_t.Size = new System.Drawing.Size(108, 13);
+            this.cust_surname_company_t.TabIndex = 41;
+            this.cust_surname_company_t.Text = "Surname / Company";
+            this.cust_surname_company_t.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // cust_initials_t
+            // 
+            this.cust_initials_t.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cust_initials_t.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cust_initials_t.ForeColor = System.Drawing.Color.Black;
+            this.cust_initials_t.Location = new System.Drawing.Point(3, 24);
+            this.cust_initials_t.Name = "cust_initials_t";
+            this.cust_initials_t.Size = new System.Drawing.Size(105, 13);
+            this.cust_initials_t.TabIndex = 42;
+            this.cust_initials_t.Text = "Initials / Firstnames";
+            this.cust_initials_t.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // DAddressSearchPrimContact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.cust_surname_company);
+            this.Controls.Add(this.cust_initials);
+            this.Controls.Add(this.cust_surname_company_t);
+            this.Controls.Add(this.cust_initials_t);
+            this.Name = "DAddressSearchPrimContact";
+            this.Size = new System.Drawing.Size(601, 104);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
-			//
-			// cust_initials
-			//
-			cust_initials = new System.Windows.Forms.TextBox();
-			this.cust_initials.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-			this.cust_initials.Font = new System.Drawing.Font("MS Sans Serif", 8F);
-			this.cust_initials.ForeColor = System.Drawing.Color.Black;
-			this.cust_initials.Location = new System.Drawing.Point(110, 23);
-			this.cust_initials.MaxLength = 30;
-			this.cust_initials.Name = "cust_initials";
-			this.cust_initials.Size = new System.Drawing.Size(230, 13);
-			this.cust_initials.TextAlign = HorizontalAlignment.Left;
-			this.cust_initials.DataBindings.Add(new System.Windows.Forms.Binding("Text",
-				this.bindingSource, "CustInitials", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-			this.cust_initials.TabIndex = 40;
-			this.Controls.Add(cust_initials);
-
-			//
-			// cust_surname_company_t
-			//
-			this.cust_surname_company_t = new System.Windows.Forms.Label();
-			this.cust_surname_company_t.Font = new System.Drawing.Font("MS Sans Serif", 8F);
-			this.cust_surname_company_t.ForeColor = System.Drawing.Color.Black;
-			this.cust_surname_company_t.Location = new System.Drawing.Point(0, 1);
-			this.cust_surname_company_t.Name = "cust_surname_company_t";
-			this.cust_surname_company_t.Size = new System.Drawing.Size(108, 13);
-			this.cust_surname_company_t.Text = "Surname / Company";
-			this.cust_surname_company_t.TextAlign = ContentAlignment.MiddleRight;
-			this.cust_surname_company_t.BackColor = System.Drawing.SystemColors.ButtonFace;
-			this.Controls.Add(cust_surname_company_t);
-
-			//
-			// cust_initials_t
-			//
-			this.cust_initials_t = new System.Windows.Forms.Label();
-			this.cust_initials_t.Font = new System.Drawing.Font("MS Sans Serif", 8F);
-			this.cust_initials_t.ForeColor = System.Drawing.Color.Black;
-			this.cust_initials_t.Location = new System.Drawing.Point(3, 22);
-			this.cust_initials_t.Name = "cust_initials_t";
-			this.cust_initials_t.Size = new System.Drawing.Size(105, 13);
-			this.cust_initials_t.Text = "Initials / Firstnames";
-			this.cust_initials_t.TextAlign = ContentAlignment.MiddleRight;
-			this.cust_initials_t.BackColor = System.Drawing.SystemColors.ButtonFace;
-			this.Controls.Add(cust_initials_t);
-
-			this.Size = new System.Drawing.Size(650, 300);
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
-			this.ResumeLayout(false);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 		}
 		#endregion
 
