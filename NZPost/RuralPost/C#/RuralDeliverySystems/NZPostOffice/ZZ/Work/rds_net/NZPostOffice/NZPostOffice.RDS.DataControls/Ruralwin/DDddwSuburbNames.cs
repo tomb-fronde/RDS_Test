bc3d8e489ec5248dbe47cd56e79cbbb15452d10d
Brittont <brittont@fronde.com>
2010-02-01 04:13:29 +0000

    TJB RD7_0042        Removed unneeded commented-out code

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruralwin/DDddwSuburbNames.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruralwin/DDddwSuburbNames.cs
@@ -17,55 +17,6 @@ namespace NZPostOffice.RDS.DataControls.Ruralwin
 			InitializeComponent();
 		}
         
-        
-        //public void FilterMisc(string lsFilter) 
-        //{           
-
-        //    DddwSuburbNames[] entitySource = DddwSuburbNames.GetAllDddwSuburbNames();
-        //    stringSource.Clear();
-        //    foreach (DddwSuburbNames item in entitySource)
-        //    {
-        //        stringSource.Add(item.SlName);
-        //    }
-
-
-        //    if (!string.IsNullOrEmpty(lsFilter))
-        //    {
-        //        List<string> filteredStringSource = new List<string>();
-
-        //        string[] items = lsFilter.Split(new char[] { ',' });
-
-        //        foreach (string item in stringSource)
-        //        {
-        //            foreach (string s in items)//! allow the empty value added 
-        //            {
-        //                if (item == s && this.NotRepeatedItem(filteredStringSource, item))//! do not add one item more than once
-        //                {
-        //                    filteredStringSource.Add(item);
-        //                }
-        //            }
-        //        }
-        //        filteredStringSource.Add("");//! add an empty string at end of the list
-
-        //        this.bindingSource.CurrencyManager.SuspendBinding();
-        //        this.bindingSource.DataSource = filteredStringSource;//! filter
-        //        this.bindingSource.CurrencyManager.ResumeBinding();
-        //        //! stringSource = filteredStringSource;
-        //    }           
-        //}
-
-        //private bool NotRepeatedItem(List<string> filteredStringSource, string item) 
-        //{
-        //    foreach (string i in filteredStringSource)
-        //    {
-        //        if (i == item)
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //    return true;
-        //}
-
         List<DddwSuburbNames> stringSource = null;
 		public override int Retrieve()
         {		   
@@ -81,13 +32,10 @@ namespace NZPostOffice.RDS.DataControls.Ruralwin
             {
                 stringSource.Add(item.SlName);
             }
-            stringSource.Add("");//! add an empty string at end of the list
-
+            //! add an empty string at end of the list
+            stringSource.Add("");
             this.bindingSource.DataSource = stringSource;
-
             return stringSource.Count;
-          
-            //!return RetrieveCore<DddwSuburbNames>(DddwSuburbNames.GetAllDddwSuburbNames());
 		}
 	}
 }
