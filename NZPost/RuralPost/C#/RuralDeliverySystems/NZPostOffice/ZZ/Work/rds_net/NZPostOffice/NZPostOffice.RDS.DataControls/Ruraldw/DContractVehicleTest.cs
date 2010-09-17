0d9a2c719a29b0348ca082008c98eefca76d3b93
Brittont <brittont@fronde.com>
2010-09-17 05:29:31 +0000

    TJB RPI_013         Re-instated from v7.1.2 and updated is v7.1.3 functionality           to fix window scrolling

---
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DContractVehicleTest.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using NZPostOffice.RDS.Entity.Ruraldw;
+
+namespace NZPostOffice.RDS.DataControls.Ruraldw
+{
+    public partial class DContractVehicle : Metex.Windows.DataUserControl
+    {
+        public DContractVehicle()
+        {
+            InitializeComponent();
+        }
+
+        public int Retrieve(int? contract_no, int? contract_seq_number)
+        {
+            retrieve_end = false;
+            return RetrieveCore<ContractVehicle>(new List<ContractVehicle>
+                (ContractVehicle.GetAllContractVehicle(contract_no, contract_seq_number)));
+        }
+    }
+}
