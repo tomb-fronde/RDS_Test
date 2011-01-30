2c1d04d514e4b0682c3b11cde229798029e12f19
Brittont <brittont@fronde.com>
2011-01-30 20:55:49 +0000

    TJB  Sequencing Review         Modified retrieval - removed frequency details (since now         sequencing is by contract, not contract+frequency.

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DSeqAddresses.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DSeqAddresses.cs
@@ -10,15 +10,19 @@ using NZPostOffice.RDS.Entity.Ruraldw;
 
 namespace NZPostOffice.RDS.DataControls.Ruraldw
 {
-	public partial class DSeqAddresses : Metex.Windows.DataUserControl
-	{
-		public DSeqAddresses()
-		{
-			InitializeComponent();
+    public partial class DSeqAddresses : Metex.Windows.DataUserControl
+    {
+        // TJB  Jan-2011  Sequencing Review
+        // Modified retrieval - removed frequency details (since now sequencing
+        // is by contract, not contract+frequency.
+
+        public DSeqAddresses()
+        {
+            InitializeComponent();
 
             grid.CellClick += new DataGridViewCellEventHandler(grid_CellClick);
             grid.CellMouseDown += new DataGridViewCellMouseEventHandler(grid_CellMouseDown);
-		}
+        }
 
         void grid_CellMouseDown(object sender, DataGridViewCellMouseEventArgs e)
         {
@@ -29,20 +33,20 @@ namespace NZPostOffice.RDS.DataControls.Ruraldw
         void grid_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (CellClick != null)
-                CellClick(sender,e);
+                CellClick(sender, e);
         }
 
-		public int Retrieve( int? al_contract_no, int? al_sf_key, string as_rf_delivery_days )
-		{
-			int rowCount = RetrieveCore<SeqAddresses>(new List<SeqAddresses>
-				(SeqAddresses.GetAllSeqAddresses(al_contract_no, al_sf_key, as_rf_delivery_days )));
-            if(rowCount>0)
+        public int Retrieve(int? al_contract_no)
+        {
+            int rowCount = RetrieveCore<SeqAddresses>(new List<SeqAddresses>
+                            (SeqAddresses.GetAllSeqAddresses(al_contract_no)));
+            if (rowCount > 0)
                 this.SortString = "seq_num A";
-                this.Sort<SeqAddresses>();
+            this.Sort<SeqAddresses>();
             return rowCount;
-		}
+        }
 
         public event EventHandler CellClick;
         public event DataGridViewCellMouseEventHandler CellMouseDown;
-	}
+    }
 }
--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DSeqAddresses.designer.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DSeqAddresses.designer.cs
@@ -1,172 +1,145 @@
 namespace NZPostOffice.RDS.DataControls.Ruraldw
 {
-	partial class DSeqAddresses
-	{
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
-		private System.ComponentModel.IContainer components = null;
-		private System.Windows.Forms.DataGridViewTextBoxColumn  adr_num_alpha;
-		private System.Windows.Forms.DataGridViewTextBoxColumn  customer;
-		private System.Windows.Forms.DataGridViewTextBoxColumn  sequence_no;
-		private System.Windows.Forms.DataGridViewTextBoxColumn  road_name;
+    partial class DSeqAddresses
+    {
+        // TJB  Sequencing Review  Jan-2011
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
         private System.Windows.Forms.DataGridViewTextBoxColumn address_adr_alpha;
         private System.Windows.Forms.DataGridViewTextBoxColumn address_frequency_sequence_delivery_sequence;
 
-	
-		protected override void Dispose(bool disposing)
-		{
-			if (disposing && (components != null ))
-			{
-				components.Dispose();
-			}
-			base.Dispose(disposing);
-		}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-		private Metex.Windows.DataEntityGrid grid;
-		#region Component Designer generated code
-		private void InitializeComponent()
-		{
-			components = new System.ComponentModel.Container();
-			grid = new Metex.Windows.DataEntityGrid();
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
-			this.SuspendLayout();
+        private Metex.Windows.DataEntityGrid grid;
 
-			// 
-			// bindingSource
-			//
-			this.bindingSource.DataSource = typeof(NZPostOffice.RDS.Entity.Ruraldw.SeqAddresses);
-
-			// 
-			// grid
-			// 
-			this.grid.AllowUserToAddRows = false;
-			this.grid.AllowUserToResizeRows = false;
-			this.grid.AutoGenerateColumns = false;
-            this.grid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+        #region Component Designer generated code
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grid = new Metex.Windows.DataEntityGrid();
+            this.sequence_no = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.adr_num_alpha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.road_name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bindingSource
+            // 
+            this.bindingSource.DataSource = typeof(NZPostOffice.RDS.Entity.Ruraldw.SeqAddresses);
+            this.bindingSource.ListChanged += new System.ComponentModel.ListChangedEventHandler(this.bindingSource_ListChanged);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.AutoGenerateColumns = false;
             this.grid.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
-			this.grid.ColumnHeadersDefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-			this.grid.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.SystemColors.Control;
-			this.grid.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Arial", 8.25F);
-			this.grid.ColumnHeadersDefaultCellStyle.ForeColor = System.Drawing.SystemColors.WindowText;
-			this.grid.ColumnHeadersDefaultCellStyle.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-			this.grid.ColumnHeadersDefaultCellStyle.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-			this.grid.ColumnHeadersDefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-			this.grid.ColumnHeadersHeight = 28;
-			this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-			this.grid.DataSource = this.bindingSource;
-			this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-			this.grid.Location = new System.Drawing.Point(0, 0);
-			this.grid.MultiSelect = true;
-			this.grid.Name = "grid";
-			this.grid.RowHeadersVisible = false;
-			this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.grid.Size = new System.Drawing.Size(638, 252);
-			this.grid.TabIndex = 0;
+            this.grid.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 8.25F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid.ColumnHeadersHeight = 28;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.sequence_no,
+            this.adr_num_alpha,
+            this.road_name,
+            this.customer});
+            this.grid.DataSource = this.bindingSource;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "grid";
+            this.grid.RowHeadersVisible = false;
+            this.grid.RowTemplate.Height = 16;
             this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.grid.MultiSelect = true;
-            this.bindingSource.ListChanged += new System.ComponentModel.ListChangedEventHandler(bindingSource_ListChanged);
-            //
+            this.grid.Size = new System.Drawing.Size(638, 252);
+            this.grid.TabIndex = 0;
+            // 
             // sequence_no
-            //
-            sequence_no = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            // 
             this.sequence_no.DataPropertyName = "SequenceNo";
-            this.sequence_no.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            this.sequence_no.DefaultCellStyle.BackColor = System.Drawing.SystemColors.Control;//.ColorTranslator.FromWin32(80269524);
-            this.sequence_no.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
-            this.sequence_no.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            this.sequence_no.DefaultCellStyle = dataGridViewCellStyle2;
             this.sequence_no.HeaderText = "Seq";
             this.sequence_no.Name = "sequence_no";
             this.sequence_no.Width = 28;
-            this.grid.Columns.Add(sequence_no);
-
-            //
+            // 
             // adr_num_alpha
-            //
-            adr_num_alpha = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            // 
             this.adr_num_alpha.DataPropertyName = "AdrNumAlpha";
-            this.adr_num_alpha.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            this.adr_num_alpha.DefaultCellStyle.BackColor = System.Drawing.SystemColors.Control;//.ColorTranslator.FromWin32(80269524);
-            this.adr_num_alpha.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
-            this.adr_num_alpha.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            this.adr_num_alpha.DefaultCellStyle = dataGridViewCellStyle3;
             this.adr_num_alpha.HeaderText = "Road Num";
             this.adr_num_alpha.Name = "adr_num_alpha";
             this.adr_num_alpha.ReadOnly = true;
             this.adr_num_alpha.Width = 68;
-            this.grid.Columns.Add(adr_num_alpha);
-
-			
-			//
-			// road_name
-			//
-			road_name= new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.road_name.DataPropertyName = "RoadName";
-			this.road_name.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            this.road_name.DefaultCellStyle.BackColor = System.Drawing.SystemColors.Control;//.ColorTranslator.FromWin32(80269524);
-			this.road_name.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
-			this.road_name.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
-			this.road_name.HeaderText = "Road Name";
-			this.road_name.Name = "road_name";
-			this.road_name.ReadOnly = true;
-			this.road_name.Width = 161;
-			this.grid.Columns.Add(road_name);
-
-
-            //
+            // 
+            // road_name
+            // 
+            this.road_name.DataPropertyName = "RoadName";
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            this.road_name.DefaultCellStyle = dataGridViewCellStyle4;
+            this.road_name.HeaderText = "Road Name";
+            this.road_name.Name = "road_name";
+            this.road_name.ReadOnly = true;
+            this.road_name.Width = 161;
+            // 
             // customer
-            //
-            customer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            // 
             this.customer.DataPropertyName = "Customer";
-            this.customer.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            this.customer.DefaultCellStyle.BackColor = System.Drawing.SystemColors.Control;//.ColorTranslator.FromWin32(80269524);
-            this.customer.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
-            this.customer.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.Black;
+            this.customer.DefaultCellStyle = dataGridViewCellStyle5;
             this.customer.HeaderText = "Customer";
             this.customer.Name = "customer";
             this.customer.ReadOnly = true;
             this.customer.Width = 224;
-            this.grid.Columns.Add(customer);
+            // 
+            // DSeqAddresses
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.grid);
+            this.Name = "DSeqAddresses";
+            this.Size = new System.Drawing.Size(638, 252);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
 
-            //
-            // address_adr_alpha
-            //
-            address_adr_alpha = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.address_adr_alpha.DataPropertyName = "AddressAdrAlpha";
-            this.address_adr_alpha.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            //this.address_adr_alpha.DefaultCellStyle.BackColor = System.Drawing.ColorTranslator.FromWin32(80269524);
-            this.address_adr_alpha.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
-            this.address_adr_alpha.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
-            this.address_adr_alpha.HeaderText = "";
-            this.address_adr_alpha.Name = "address_adr_alpha";
-            this.address_adr_alpha.ReadOnly = true;
-            this.address_adr_alpha.Width = 124;
-            this.address_adr_alpha.Visible = false;
-            this.grid.Columns.Add(address_adr_alpha);
-            //
-            // address_frequency_sequence_delivery_sequence
-            //
-            address_frequency_sequence_delivery_sequence = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.address_frequency_sequence_delivery_sequence.DataPropertyName = "AddressFrequencySequenceDeliverySequence";
-            this.address_frequency_sequence_delivery_sequence.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            this.address_frequency_sequence_delivery_sequence.DefaultCellStyle.BackColor = System.Drawing.ColorTranslator.FromWin32(80269524);
-            this.address_frequency_sequence_delivery_sequence.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
-            this.address_frequency_sequence_delivery_sequence.DefaultCellStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
-            this.address_frequency_sequence_delivery_sequence.HeaderText = "";
-            this.address_frequency_sequence_delivery_sequence.Name = "address_frequency_sequence_delivery_sequence";
-            this.address_frequency_sequence_delivery_sequence.ReadOnly = true;
-            this.address_frequency_sequence_delivery_sequence.Width = 224;
-            this.address_frequency_sequence_delivery_sequence.Visible = false;
-            this.grid.Columns.Add(address_frequency_sequence_delivery_sequence);
-
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
-			this.ResumeLayout(false); this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Size = new System.Drawing.Size(638, 252);
-			//this.BackColor = System.Drawing.ColorTranslator.FromWin32(80269524);
-			this.Controls.Add(grid);
-		}
+        }
 
         void bindingSource_ListChanged(object sender, System.ComponentModel.ListChangedEventArgs e)
         {
@@ -179,7 +152,12 @@ namespace NZPostOffice.RDS.DataControls.Ruraldw
             }
             this.bindingSource.RaiseListChangedEvents = true;
         }
-		#endregion
+        #endregion
 
-	}
+        private System.Windows.Forms.DataGridViewTextBoxColumn sequence_no;
+        private System.Windows.Forms.DataGridViewTextBoxColumn adr_num_alpha;
+        private System.Windows.Forms.DataGridViewTextBoxColumn road_name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customer;
+
+    }
 }
