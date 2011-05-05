303baad5b24ee30843a0cd2f696e903661442cdd
Brittont <brittont@fronde.com>
2011-05-05 23:45:14 +0000

    Vss2Git

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDSAdmin.DataControls/DOutletType.Designer.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDSAdmin.DataControls/DOutletType.Designer.cs
@@ -22,63 +22,69 @@ namespace NZPostOffice.RDSAdmin.DataControls.Security
 		#region Component Designer generated code
 		private void InitializeComponent()
 		{
-			components = new System.ComponentModel.Container();
-			grid = new Metex.Windows.DataEntityGrid();
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
-			this.SuspendLayout();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.grid = new Metex.Windows.DataEntityGrid();
+            this.ot_outlet_type = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bindingSource
+            // 
+            this.bindingSource.DataSource = typeof(NZPostOffice.RDSAdmin.Entity.Security.OutletType);
+            // 
+            // grid
+            // 
+            this.grid.AllowUserToAddRows = false;
+            this.grid.AllowUserToResizeRows = false;
+            this.grid.AutoGenerateColumns = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 8.25F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.grid.ColumnHeadersHeight = 28;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.grid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ot_outlet_type});
+            this.grid.DataSource = this.bindingSource;
+            this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.grid.Location = new System.Drawing.Point(0, 0);
+            this.grid.Name = "grid";
+            this.grid.RowHeadersVisible = false;
+            this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grid.Size = new System.Drawing.Size(638, 347);
+            this.grid.TabIndex = 0;
+            // 
+            // ot_outlet_type
+            // 
+            this.ot_outlet_type.DataPropertyName = "OtOutletType";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            this.ot_outlet_type.DefaultCellStyle = dataGridViewCellStyle2;
+            this.ot_outlet_type.HeaderText = "Outlet Types";
+            this.ot_outlet_type.Name = "ot_outlet_type";
+            this.ot_outlet_type.Width = 176;
+            // 
+            // DOutletType
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(212)))), ((int)(((byte)(208)))), ((int)(((byte)(200)))));
+            this.Controls.Add(this.grid);
+            this.Name = "DOutletType";
+            this.Size = new System.Drawing.Size(638, 347);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
 
-			// 
-			// bindingSource
-			//
-			this.bindingSource.DataSource = typeof(NZPostOffice.RDSAdmin.Entity.Security.OutletType);
-
-			// 
-			// grid
-			// 
-			this.grid.AllowUserToAddRows = false;
-			this.grid.AllowUserToResizeRows = false;
-			this.grid.AutoGenerateColumns = false;
-			this.grid.ColumnHeadersDefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
-			this.grid.ColumnHeadersDefaultCellStyle.BackColor = System.Drawing.SystemColors.Control;
-			this.grid.ColumnHeadersDefaultCellStyle.Font = new System.Drawing.Font("Arial", 8.25F);
-			this.grid.ColumnHeadersDefaultCellStyle.ForeColor = System.Drawing.SystemColors.WindowText;
-			this.grid.ColumnHeadersDefaultCellStyle.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-			this.grid.ColumnHeadersDefaultCellStyle.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-			this.grid.ColumnHeadersDefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
-			this.grid.ColumnHeadersHeight = 28;
-			this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-			this.grid.DataSource = this.bindingSource;
-			this.grid.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.grid.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-			this.grid.Location = new System.Drawing.Point(0, 0);
-			this.grid.MultiSelect = true;
-			this.grid.Name = "grid";
-			this.grid.RowHeadersVisible = false;
-			this.grid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-			this.grid.Size = new System.Drawing.Size(638, 252);
-			this.grid.TabIndex = 0;
-
-			//
-			// ot_outlet_type
-			//
-			ot_outlet_type= new System.Windows.Forms.DataGridViewTextBoxColumn();
-			this.ot_outlet_type.DataPropertyName = "OtOutletType";
-			this.ot_outlet_type.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-			this.ot_outlet_type.DefaultCellStyle.BackColor = System.Drawing.ColorTranslator.FromWin32(1090519039);
-			this.ot_outlet_type.DefaultCellStyle.ForeColor = System.Drawing.Color.Black;
-			this.ot_outlet_type.DefaultCellStyle.Font = new System.Drawing.Font("MS Sans Serif", 8F);
-			this.ot_outlet_type.HeaderText = "Outlet Types";
-			this.ot_outlet_type.Name = "ot_outlet_type";
-			this.ot_outlet_type.Width = 176;
-			this.grid.Columns.Add(ot_outlet_type);
-
-			((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
-			this.ResumeLayout(false); this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Size = new System.Drawing.Size(638, 252);
-			this.BackColor = System.Drawing.ColorTranslator.FromWin32(80269524);
-			this.Controls.Add(grid);
 		}
 		#endregion
 
