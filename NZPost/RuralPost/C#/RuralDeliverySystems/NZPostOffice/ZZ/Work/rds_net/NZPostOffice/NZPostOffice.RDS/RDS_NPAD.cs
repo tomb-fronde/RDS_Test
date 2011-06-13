ad5026c963bdee375de3069609d159b83f74eaa7
Brittont <brittont@fronde.com>
2011-06-13 23:38:47 +0000

    TJB - Changed display of built-on date to use actual build date.

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/RDS_NPAD.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/RDS_NPAD.cs
@@ -11,23 +11,30 @@ namespace NZPostOffice.RDS
 {
     public class RDS_NPAD : ApplicationContext
     {
+        // TJB  May-2011
+        //      Changed 'built on' value from config file to file build timestamp
         public RDS_NPAD()
         {
             StaticVariables.CurAppContext = this;
             Cursor.Current = Cursors.WaitCursor;
             System.Configuration.Configuration config = System.Configuration.ConfigurationManager.OpenExeConfiguration(
-              System.Configuration.ConfigurationUserLevel.None);
+                System.Configuration.ConfigurationUserLevel.None);
 
             // Add an entry to appSettings.
             NZPostOffice.Shared.Security.Identity threadId = new NZPostOffice.Shared.Security.Identity(
                 config.AppSettings.Settings["DefaultLoginId"].Value.ToString(),
-               config.AppSettings.Settings["DefaultLoginPassword"].Value.ToString(), "NZPO");
+                config.AppSettings.Settings["DefaultLoginPassword"].Value.ToString(), "NZPO");
             System.Threading.Thread.CurrentPrincipal = new NZPostOffice.Shared.Security.Principal(threadId);
 
             bool ret = LoginService.TryConnectNZPO();
             LogonAttrib lg = new LogonAttrib();
             lg.appName = config.AppSettings.Settings["AppName"].Value.ToString();
-            lg.version = "Version " + config.AppSettings.Settings["Version"].Value.ToString() + " (Built on " + config.AppSettings.Settings["BuildDate"].Value.ToString() + ")";
+            // TJB  May-2011: Changed 'Built on' value to file build timestamp
+            //lg.version = "Version " + config.AppSettings.Settings["Version"].Value.ToString() + " (Built on " + config.AppSettings.Settings["BuildDate"].Value.ToString() + ")";
+            lg.version = "Version " + config.AppSettings.Settings["Version"].Value.ToString() 
+                         + " (Built on " 
+                         + string.Format("{0:dd-MMM-yyyy HH:mm}", System.IO.File.GetLastWriteTime(System.Reflection.Assembly.GetEntryAssembly().Location))
+                         + ")";
             lg.userID = config.AppSettings.Settings["DefaultUserName"].Value.ToString();
             WLogon wLogonDlg = new WLogon(lg);
             if (wLogonDlg.ShowDialog() == DialogResult.Yes)
