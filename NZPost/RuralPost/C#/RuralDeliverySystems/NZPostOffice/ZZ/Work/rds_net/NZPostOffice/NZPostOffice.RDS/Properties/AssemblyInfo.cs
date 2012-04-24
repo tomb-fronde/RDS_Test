b1d6857ddc59f2e54da9e93eea9c59535a0ff487
Brittont <brittont@fronde.com>
2012-04-24 20:44:16 +0000

    Vss2Git

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Properties/AssemblyInfo.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fronde Systems Group")]
 [assembly: AssemblyProduct("Rural Delivery System")]
-[assembly: AssemblyCopyright("Copyright@2011 Rural Post")]
+[assembly: AssemblyCopyright("Copyright@2012 Rural Post")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.1.7.3")]
-[assembly: AssemblyFileVersion("7.1.7.3")]
+[assembly: AssemblyVersion("7.1.8.0")]
+[assembly: AssemblyFileVersion("7.1.8.0")]
