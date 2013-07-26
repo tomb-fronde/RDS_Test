f603d7aa4b17fb42eb8d2220a4536eda005cd96c
Brittont <brittont@fronde.com>
2013-07-26 04:53:51 +0000

    TJB Updated version to 7.1.10.1

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPost.RDSAdmin/Properties/AssemblyInfo.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPost.RDSAdmin/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.1.4.0")]
-[assembly: AssemblyFileVersion("7.1.4.0")]
+[assembly: AssemblyVersion("7.1.10.1")]
+[assembly: AssemblyFileVersion("7.1.10.1")]
