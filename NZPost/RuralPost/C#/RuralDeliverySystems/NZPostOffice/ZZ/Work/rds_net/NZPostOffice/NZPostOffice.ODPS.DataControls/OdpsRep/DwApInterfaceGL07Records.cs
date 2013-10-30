9dab26ba9ea218766722884c7492dd0d3c5ccda0
Brittont <brittont@fronde.com>
2013-10-30 02:25:15 +0000

    TJB New NZ Post FMIS system   30-Oct-2013: NEW       Datawindow for new AP Export file format

---
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsRep/DwApInterfaceGL07Records.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Metex.Core;
+using NZPostOffice.ODPS.Entity.OdpsRep;
+
+namespace NZPostOffice.ODPS.DataControls.OdpsRep
+{
+    public partial class DwApInterfaceGL07Records : Metex.Windows.DataUserControl
+    {
+        public DwApInterfaceGL07Records()
+        {
+            InitializeComponent();
+        }
+        
+        public int Retrieve(DateTime? ProcDate)
+        {
+            return RetrieveCore<ApInterfaceGL07Records>(new List<ApInterfaceGL07Records>
+                (ApInterfaceGL07Records.GetAllApInterfaceGL07Records(ProcDate)));
+        }
+        
+    }
+}
