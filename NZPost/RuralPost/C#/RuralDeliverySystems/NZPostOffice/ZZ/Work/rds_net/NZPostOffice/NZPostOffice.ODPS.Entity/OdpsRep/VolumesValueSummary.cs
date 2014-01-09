b5739f9fed3b7074e49962a29f78a12d85b609c2
Brittont <brittont@fronde.com>
2014-01-09 08:27:00 +0000

    TJB  RPCR_059  Jan-2014     Added Prs5name, Prs5volume, Prs5Value     Removed Adpost, Courierpost, Skyroad, Parcelpost            volume and values; replaced with Prs<n> volume & value     Added Prs[1-4]name     Moved Conttype to follow Name     See OD_RPS_VolumesValues_Summary.sql     See also DPS.DataControls.Report             VolumesValueSummaryDataset

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.Entity/OdpsRep/VolumesValueSummary.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.Entity/OdpsRep/VolumesValueSummary.cs
@@ -8,22 +8,38 @@ using Metex.Core.Security;
 
 namespace NZPostOffice.ODPS.Entity.OdpsRep
 {
-	// Mapping info for object fields to DB
+    // TJB  RPCR_059  Jan-2014
+    // Added Prs5name, Prs5volume, Prs5Value
+    // Removed Adpost, Courierpost, Skyroad, Parcelpost volumes and values
+    // replaced with Prs<n> volume & value
+    // Added Prs[1-4]name
+    // Moved Conttype to follow Name
+    // See OD_RPS_VolumesValues_Summary.sql
+    
+    // Mapping info for object fields to DB
 	// Mapping fieldname, entity fieldname, database table name, form name
 	// Application Form Name : BE
 	[MapInfo("contract_no", "_contract_no", "")]
-	[MapInfo("region", "_region", "")]
-	[MapInfo("name", "_c_surname_company", "")]
-	[MapInfo("adpostvolume", "_adpostvolume", "")]
-	[MapInfo("adpostvalue", "_adpostvalue", "")]
-	[MapInfo("courierpostvolume", "_courierpostvolume", "")]
-	[MapInfo("courierpostvalue", "_courierpostvalue", "")]
-	[MapInfo("xpvolume", "_xpvolume", "")]
-	[MapInfo("xpvalue", "_xpvalue", "")]
-	[MapInfo("conttype", "_conttype", "")]
-	[MapInfo("parcelpostvolume", "_parcelpostvolume", "")]
-	[MapInfo("parcelpostvalue", "_parcelpostvalue", "")]
-	[System.Serializable()]
+	[MapInfo("region",     "_region", "")]
+	[MapInfo("name",       "_c_surname_company", "")]
+    [MapInfo("conttype",   "_conttype", "")]
+    [MapInfo("prs1name",   "_prs1name", "")]
+    [MapInfo("prs1volume", "_prs1volume", "")]
+    [MapInfo("prs1value",  "_prs1value", "")]
+    [MapInfo("prs2name",   "_prs2name", "")]
+    [MapInfo("prs2volume", "_prs2volume", "")]
+    [MapInfo("prs2value",  "_prs2value", "")]
+    [MapInfo("prs3name",   "_prs3name", "")]
+    [MapInfo("prs3volume", "_prs3volume", "")]
+    [MapInfo("prs3value",  "_prs3value", "")]
+    [MapInfo("prs4name",   "_prs4name", "")]
+    [MapInfo("prs4volume", "_prs4volume", "")]
+    [MapInfo("prs4value",  "_prs4value", "")]
+    [MapInfo("prs5name",   "_prs5name", "")]
+    [MapInfo("prs5volume", "_prs5volume", "")]
+    [MapInfo("prs5value",  "_prs5value", "")]
+    
+    [System.Serializable()]
 
 	public class VolumesValueSummary : Entity<VolumesValueSummary>
 	{
@@ -37,34 +53,55 @@ namespace NZPostOffice.ODPS.Entity.OdpsRep
 		[DBField()]
 		private string  _c_surname_company;
 
-		[DBField()]
-		private decimal?  _adpostvolume;
+        [DBField()]
+        private string _conttype;
+
+        [DBField()]
+        private string _prs1name;
+
+        [DBField()]
+		private decimal?  _prs1volume;
 
 		[DBField()]
-		private decimal?  _adpostvalue;
+        private decimal? _prs1value;
 
-		[DBField()]
-		private decimal?  _courierpostvolume;
+        [DBField()]
+        private string _prs2name;
 
-		[DBField()]
-		private decimal?  _courierpostvalue;
+        [DBField()]
+        private decimal? _prs2volume;
 
-		[DBField()]
-		private decimal?  _xpvolume;
+        [DBField()]
+        private decimal? _prs2value;
 
-		[DBField()]
-		private decimal?  _xpvalue;
+        [DBField()]
+        private string _prs3name;
 
-		[DBField()]
-		private string  _conttype;
+        [DBField()]
+        private decimal? _prs3volume;
 
-		[DBField()]
-		private decimal?  _parcelpostvolume;
+        [DBField()]
+        private decimal? _prs3value;
 
-		[DBField()]
-		private decimal?  _parcelpostvalue;
+        [DBField()]
+        private string _prs4name;
 
-		public virtual int? ContractNo
+        [DBField()]
+        private decimal? _prs4volume;
+
+        [DBField()]
+        private decimal? _prs4value;
+
+        [DBField()]
+        private string _prs5name;
+
+        [DBField()]
+        private decimal? _prs5volume;
+
+        [DBField()]
+        private decimal? _prs5value;
+
+        public virtual int? ContractNo
 		{
 			get
 			{
@@ -118,169 +155,295 @@ namespace NZPostOffice.ODPS.Entity.OdpsRep
 			}
 		}
 
-		public virtual decimal? Adpostvolume
-		{
-			get
-			{
-				CanReadProperty("Adpostvolume",true);
-				return _adpostvolume;
-			}
-			set
-			{
-				CanWriteProperty("Adpostvolume",true);
-				if ( _adpostvolume != value )
-				{
-					_adpostvolume = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual string Conttype
+        {
+            get
+            {
+                CanReadProperty("Conttype", true);
+                return _conttype;
+            }
+            set
+            {
+                CanWriteProperty("Conttype", true);
+                if (_conttype != value)
+                {
+                    _conttype = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual decimal? Adpostvalue
-		{
-			get
-			{
-				CanReadProperty("Adpostvalue",true);
-				return _adpostvalue;
-			}
-			set
-			{
-				CanWriteProperty("Adpostvalue",true);
-				if ( _adpostvalue != value )
-				{
-					_adpostvalue = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual string Prs1name
+        {
+            get
+            {
+                CanReadProperty("Prs1name", true);
+                return _prs1name;
+            }
+            set
+            {
+                CanWriteProperty("Prs1name", true);
+                if (_prs1name != value)
+                {
+                    _prs1name = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual decimal? Courierpostvolume
-		{
-			get
-			{
-				CanReadProperty("Courierpostvolume",true);
-				return _courierpostvolume;
-			}
-			set
-			{
-				CanWriteProperty("Courierpostvolume",true);
-				if ( _courierpostvolume != value )
-				{
-					_courierpostvolume = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual decimal? Prs1volume
+        {
+            get
+            {
+                CanReadProperty("Prs1volume", true);
+                return _prs1volume;
+            }
+            set
+            {
+                CanWriteProperty("Prs1volume", true);
+                if (_prs1volume != value)
+                {
+                    _prs1volume = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual decimal? Courierpostvalue
-		{
-			get
-			{
-				CanReadProperty("Courierpostvalue",true);
-				return _courierpostvalue;
-			}
-			set
-			{
-				CanWriteProperty("Courierpostvalue",true);
-				if ( _courierpostvalue != value )
-				{
-					_courierpostvalue = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual decimal? Prs1value
+        {
+            get
+            {
+                CanReadProperty("Prs1value", true);
+                return _prs1value;
+            }
+            set
+            {
+                CanWriteProperty("Prs1value", true);
+                if (_prs1value != value)
+                {
+                    _prs1value = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual decimal? Xpvolume
-		{
-			get
-			{
-				CanReadProperty("Xpvolume",true);
-				return _xpvolume;
-			}
-			set
-			{
-				CanWriteProperty("Xpvolume",true);
-				if ( _xpvolume != value )
-				{
-					_xpvolume = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual string Prs2name
+        {
+            get
+            {
+                CanReadProperty("Prs2name", true);
+                return _prs2name;
+            }
+            set
+            {
+                CanWriteProperty("Prs2name", true);
+                if (_prs2name != value)
+                {
+                    _prs2name = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual decimal? Xpvalue
-		{
-			get
-			{
-				CanReadProperty("Xpvalue",true);
-				return _xpvalue;
-			}
-			set
-			{
-				CanWriteProperty("Xpvalue",true);
-				if ( _xpvalue != value )
-				{
-					_xpvalue = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual decimal? Prs2volume
+        {
+            get
+            {
+                CanReadProperty("Prs2volume", true);
+                return _prs2volume;
+            }
+            set
+            {
+                CanWriteProperty("Prs2volume", true);
+                if (_prs2volume != value)
+                {
+                    _prs2volume = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual string Conttype
-		{
-			get
-			{
-				CanReadProperty("Conttype",true);
-				return _conttype;
-			}
-			set
-			{
-				CanWriteProperty("Conttype",true);
-				if ( _conttype != value )
-				{
-					_conttype = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual decimal? Prs2value
+        {
+            get
+            {
+                CanReadProperty("Prs2value", true);
+                return _prs2value;
+            }
+            set
+            {
+                CanWriteProperty("Prs2value", true);
+                if (_prs2value != value)
+                {
+                    _prs2value = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual decimal? Parcelpostvolume
-		{
-			get
-			{
-				CanReadProperty("Parcelpostvolume",true);
-				return _parcelpostvolume;
-			}
-			set
-			{
-				CanWriteProperty("Parcelpostvolume",true);
-				if ( _parcelpostvolume != value )
-				{
-					_parcelpostvolume = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual string Prs3name
+        {
+            get
+            {
+                CanReadProperty("Prs3name", true);
+                return _prs3name;
+            }
+            set
+            {
+                CanWriteProperty("Prs3name", true);
+                if (_prs3name != value)
+                {
+                    _prs3name = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		public virtual decimal? Parcelpostvalue
-		{
-			get
-			{
-				CanReadProperty("Parcelpostvalue",true);
-				return _parcelpostvalue;
-			}
-			set
-			{
-				CanWriteProperty("Parcelpostvalue",true);
-				if ( _parcelpostvalue != value )
-				{
-					_parcelpostvalue = value;
-					PropertyHasChanged();
-				}
-			}
-		}
+        public virtual decimal? Prs3volume
+        {
+            get
+            {
+                CanReadProperty("Prs3volume", true);
+                return _prs3volume;
+            }
+            set
+            {
+                CanWriteProperty("Prs3volume", true);
+                if (_prs3volume != value)
+                {
+                    _prs3volume = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
 
-		protected override object GetIdValue()
+        public virtual decimal? Prs3value
+        {
+            get
+            {
+                CanReadProperty("Prs3value", true);
+                return _prs3value;
+            }
+            set
+            {
+                CanWriteProperty("Prs3value", true);
+                if (_prs3value != value)
+                {
+                    _prs3value = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        public virtual string Prs4name
+        {
+            get
+            {
+                CanReadProperty("Prs4name", true);
+                return _prs4name;
+            }
+            set
+            {
+                CanWriteProperty("Prs4name", true);
+                if (_prs4name != value)
+                {
+                    _prs4name = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        public virtual decimal? Prs4volume
+        {
+            get
+            {
+                CanReadProperty("Prs4volume", true);
+                return _prs4volume;
+            }
+            set
+            {
+                CanWriteProperty("Prs4volume", true);
+                if (_prs4volume != value)
+                {
+                    _prs4volume = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        public virtual decimal? Prs4value
+        {
+            get
+            {
+                CanReadProperty("Prs4value", true);
+                return _prs4value;
+            }
+            set
+            {
+                CanWriteProperty("Prs4value", true);
+                if (_prs4value != value)
+                {
+                    _prs4value = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        public virtual string Prs5name
+        {
+            get
+            {
+                CanReadProperty("Prs5name", true);
+                return _prs5name;
+            }
+            set
+            {
+                CanWriteProperty("Prs5name", true);
+                if (_prs5name != value)
+                {
+                    _prs5name = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        public virtual decimal? Prs5volume
+        {
+            get
+            {
+                CanReadProperty("Prs5volume", true);
+                return _prs5volume;
+            }
+            set
+            {
+                CanWriteProperty("Prs5volume", true);
+                if (_prs5volume != value)
+                {
+                    _prs5volume = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        public virtual decimal? Prs5value
+        {
+            get
+            {
+                CanReadProperty("Prs5value", true);
+                return _prs5value;
+            }
+            set
+            {
+                CanWriteProperty("Prs5value", true);
+                if (_prs5value != value)
+                {
+                    _prs5value = value;
+                    PropertyHasChanged();
+                }
+            }
+        }
+
+        protected override object GetIdValue()
 		{
 			return "";
 		}
@@ -322,16 +485,23 @@ namespace NZPostOffice.ODPS.Entity.OdpsRep
                             instance._contract_no = GetValueFromReader<Int32?>(dr, 0);
                             instance._region = GetValueFromReader<string>(dr, 1);
                             instance._c_surname_company = GetValueFromReader<string>(dr, 2);
-                            instance._adpostvolume = GetValueFromReader<decimal?>(dr, 3);
-                            instance._adpostvalue = GetValueFromReader<decimal?>(dr, 4);
-                            instance._courierpostvolume = GetValueFromReader<decimal?>(dr, 5);
-                            instance._courierpostvalue = GetValueFromReader<decimal?>(dr, 6);
-                            instance._xpvolume = GetValueFromReader<decimal?>(dr, 7);
-                            instance._xpvalue = GetValueFromReader<decimal?>(dr, 8);
-                            instance._conttype = GetValueFromReader<string>(dr, 9);
-                            instance._parcelpostvolume = GetValueFromReader<decimal?>(dr, 10);
-                            instance._parcelpostvalue = GetValueFromReader<decimal?>(dr, 11);
-							instance.MarkOld();
+                            instance._conttype = GetValueFromReader<string>(dr, 3);
+                            instance._prs1name = GetValueFromReader<string>(dr, 4);
+                            instance._prs1volume = GetValueFromReader<decimal?>(dr, 5);
+                            instance._prs1value = GetValueFromReader<decimal?>(dr, 6);
+                            instance._prs2name = GetValueFromReader<string>(dr, 7);
+                            instance._prs2volume = GetValueFromReader<decimal?>(dr, 8);
+                            instance._prs2value = GetValueFromReader<decimal?>(dr, 9);
+                            instance._prs3name = GetValueFromReader<string>(dr, 10);
+                            instance._prs3volume = GetValueFromReader<decimal?>(dr, 11);
+                            instance._prs3value = GetValueFromReader<decimal?>(dr, 12);
+                            instance._prs4name = GetValueFromReader<string>(dr, 13);
+                            instance._prs4volume = GetValueFromReader<decimal?>(dr, 14);
+                            instance._prs4value = GetValueFromReader<decimal?>(dr, 15);
+                            instance._prs5name = GetValueFromReader<string>(dr, 16);
+                            instance._prs5volume = GetValueFromReader<decimal?>(dr, 17);
+                            instance._prs5value = GetValueFromReader<decimal?>(dr, 18);
+                            instance.MarkOld();
                             instance.StoreInitialValues();
 							_list.Add(instance);
 						}
