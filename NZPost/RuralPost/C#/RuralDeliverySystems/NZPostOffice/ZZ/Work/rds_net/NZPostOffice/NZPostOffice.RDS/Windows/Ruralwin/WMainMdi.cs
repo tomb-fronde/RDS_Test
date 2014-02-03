b4ee81cbe444b325477f3aa54f1ae0838cf1594e
Brittont <brittont@fronde.com>
2014-02-03 20:19:37 +0000

    TJB  Feb 2014     Version change to 7.1.12.0, Test version

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin/WMainMdi.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin/WMainMdi.cs
@@ -39,13 +39,13 @@ namespace NZPostOffice.RDS.Windows.Ruralwin
             this.InitializeComponent();
             m_main_menu = new MMainMenu(this);
             this.Text = "Rural Delivery System";
-            this.toolStripStatusLabel4.Text = "Production Version";
+            //this.toolStripStatusLabel4.Text = "Production Version";
             //this.toolStripStatusLabel4.Text = "DEV Version";
-            //this.toolStripStatusLabel4.Text = "Test Version";
+            this.toolStripStatusLabel4.Text = "Test Version";
             // since its value was meaningless (just like the clock).
             //this.oolStripStatusLabel3.Text = "Environment 8.0.00";
-            this.toolStripStatusLabel3.Text = "RDS 7.1.11.3 ";
-            this.toolStripStatusLabel6.Text = "27-Jan-2014 11:00";
+            this.toolStripStatusLabel3.Text = "RDS 7.1.12.0 ";
+            this.toolStripStatusLabel6.Text = "04-Feb-2014 9:15";
             this.toolStripStatusLabel6.Text = string.Format("{0:dd-MMM-yyyy HH:mm}",
                        System.IO.File.GetLastWriteTime(System.Reflection.Assembly.GetEntryAssembly().Location));
         }
