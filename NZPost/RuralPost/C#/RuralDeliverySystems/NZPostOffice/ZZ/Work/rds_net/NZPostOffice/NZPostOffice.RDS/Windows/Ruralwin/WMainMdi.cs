2a6f5eb1cc2bdbf7048005e3494947b1764ccbcd
Brittont <brittont@fronde.com>
2014-05-01 02:04:50 +0000

    Vss2Git

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin/WMainMdi.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin/WMainMdi.cs
@@ -44,8 +44,8 @@ namespace NZPostOffice.RDS.Windows.Ruralwin
             this.toolStripStatusLabel4.Text = "Test Version";
             // since its value was meaningless (just like the clock).
             //this.oolStripStatusLabel3.Text = "Environment 8.0.00";
-            this.toolStripStatusLabel3.Text = "RDS 7.1.12.0 ";
-            this.toolStripStatusLabel6.Text = "04-Feb-2014 9:15";
+            this.toolStripStatusLabel3.Text = "RDS 7.1.12.2 ";
+            this.toolStripStatusLabel6.Text = "01-May-2014 14:00";
             this.toolStripStatusLabel6.Text = string.Format("{0:dd-MMM-yyyy HH:mm}",
                        System.IO.File.GetLastWriteTime(System.Reflection.Assembly.GetEntryAssembly().Location));
         }
