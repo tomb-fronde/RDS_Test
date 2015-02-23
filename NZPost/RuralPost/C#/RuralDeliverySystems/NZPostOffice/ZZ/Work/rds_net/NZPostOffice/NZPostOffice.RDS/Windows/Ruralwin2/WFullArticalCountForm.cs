3bab87b9888d0430c196f4d1e0916fb8cd3ab43d
Brittont <brittont@fronde.com>
2015-02-23 03:16:52 +0000

    TJB  RPCR_093  Feb-2015     Removed assign_to_pending button (made invisible for now).     No longer called from WContract2001; still called from WRenewal2001

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin2/WFullArticalCountForm.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin2/WFullArticalCountForm.cs
@@ -8,12 +8,16 @@ using NZPostOffice.Shared;
 
 namespace NZPostOffice.RDS.Windows.Ruralwin2
 {
+    // TJB  RPCR_093  Feb-2015
+    // Removed assign_to_pending button (made invisible for now).
+    // No longer called from WContract2001; still called from WRenewal2001
+    //
+    // TJB RPI_014 Oct-2010: (see cb_assign_to_pending_clicked)
+    // Deferred 'assign_to_pending' function to cb+ok_clicked in WArticalCountForm
+    // Fixed crash bug caused by incorrect index used.
+
     public class WFullArticalCountForm : WArticalCountForm
     {
-        // TJB RPI_014 Oct-2010: (see cb_assign_to_pending_clicked)
-        // Deferred 'assign_to_pending' function to cb+ok_clicked in WArticalCountForm
-        // Fixed crash bug caused by incorrect index used.
-
         #region Define
 
         // TJB RPI_014 Oct-2010: Added
@@ -23,6 +27,7 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
         private System.ComponentModel.IContainer components = null;
 
         public Button cb_scaling_factor;
+        private Label label1;
 
         // TJB RPI_014 Oct-2010: Changed name from "...renewal" to "...pending"
         public Button cb_assign_to_pending;
@@ -32,6 +37,8 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
         public WFullArticalCountForm()
         {
             this.InitializeComponent();
+            cb_assign_to_pending.Enabled = false;
+            cb_assign_to_pending.Visible = false;
         }
 
         public override void pfc_postopen()
@@ -42,9 +49,12 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
             int SQLCode = 0;
             string SQLErrText = string.Empty;
 
+            cb_assign_to_pending.Enabled = false;
+            cb_assign_to_pending.Visible = false;
+
+            lContract = dw_artical_count.GetItem<ContractArticalCountForm>(0).ContractNo;
             if (dw_artical_count.DataObject.GetItem<ContractArticalCountForm>(0).GetInitialValue<int?>("contract_seq_number") == null)
             {
-                lContract = dw_artical_count.GetItem<ContractArticalCountForm>(0).ContractNo;
                 //select contract_renewals.contract_seq_number
                 //  into :lRenewal 
                 //  from contract key join contract_renewals
@@ -65,7 +75,8 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
             else
                 cb_assign_to_pending.Enabled = false;
 
-            //added by jlwang
+            cb_assign_to_pending.Visible = false;
+
             dw_artical_count.DataObject.BindingSource.CurrencyManager.Refresh();
 
             //p! added to make shortcut menu happen
@@ -79,58 +90,86 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
         /// </summary>
         private void InitializeComponent()
         {
+            this.cb_scaling_factor = new System.Windows.Forms.Button();
+            this.cb_assign_to_pending = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            this.cb_scaling_factor = new Button();
-            this.cb_assign_to_pending = new Button();
-            Controls.Add(cb_scaling_factor);
-            Controls.Add(cb_assign_to_pending);
-            this.Height = 190;
-            this.Width = 550;
-            this.MinimizeBox = false;
-            this.MaximizeBox = false;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-
-            dw_artical_count.Width = 530;
+            // 
+            // dw_artical_count
+            // 
+            this.dw_artical_count.Location = new System.Drawing.Point(9, 4);
+            this.dw_artical_count.Size = new System.Drawing.Size(486, 120);
             // 
             // cb_ok
             // 
-            cb_ok.Left = 361;
-            cb_ok.Top = 130;
+            this.cb_ok.Location = new System.Drawing.Point(378, 130);
             // 
             // cb_cancel
             // 
-            cb_cancel.Left = 424;
-            cb_cancel.Top = 130;
+            this.cb_cancel.Location = new System.Drawing.Point(441, 130);
             // 
             // cb_scaling
             // 
-            cb_scaling.Tag = "ComponentName=Disabled;";
-            cb_scaling.Left = 60;
+            this.cb_scaling.Location = new System.Drawing.Point(129, 148);
+            this.cb_scaling.Tag = "ComponentName=Disabled;";
             // 
             // cb_scaling_factor
             // 
-            cb_scaling_factor.Font = new System.Drawing.Font("MS Sans Serif", 8, System.Drawing.FontStyle.Regular);
-            cb_scaling_factor.Text = "View Scaling Factor";
-            cb_scaling_factor.Tag = "ComponentName=View Factor;";
-            cb_scaling_factor.Enabled = false;
-            cb_scaling_factor.Visible = false;
-            cb_scaling_factor.TabIndex = 4;
-            cb_scaling_factor.Location = new System.Drawing.Point(3, 130);
-            cb_scaling_factor.Size = new System.Drawing.Size(109, 22);
-            cb_scaling_factor.Click += new EventHandler(cb_scaling_factor_clicked);
+            this.cb_scaling_factor.Enabled = false;
+            this.cb_scaling_factor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cb_scaling_factor.Location = new System.Drawing.Point(9, 130);
+            this.cb_scaling_factor.Name = "cb_scaling_factor";
+            this.cb_scaling_factor.Size = new System.Drawing.Size(109, 22);
+            this.cb_scaling_factor.TabIndex = 4;
+            this.cb_scaling_factor.Tag = "ComponentName=View Factor;";
+            this.cb_scaling_factor.Text = "View Scaling Factor";
+            this.cb_scaling_factor.Visible = false;
+            this.cb_scaling_factor.Click += new System.EventHandler(this.cb_scaling_factor_clicked);
             // 
             // cb_assign_to_pending
             // 
-            cb_assign_to_pending.Font = new System.Drawing.Font("MS Sans Serif", 8, System.Drawing.FontStyle.Regular);
-            cb_assign_to_pending.Text = "Assign To Pending";
-            cb_assign_to_pending.Tag = "ComponentName=Assign Article Count;";
-            cb_assign_to_pending.Enabled = false;
-            cb_assign_to_pending.Visible = false;
-            cb_assign_to_pending.TabIndex = 5;
-            cb_assign_to_pending.Location = new System.Drawing.Point(123, 130);
-            cb_assign_to_pending.Size = new System.Drawing.Size(106, 22);
-            cb_assign_to_pending.Click += new EventHandler(cb_assign_to_pending_clicked);
-            this.ResumeLayout();
+            this.cb_assign_to_pending.Enabled = false;
+            this.cb_assign_to_pending.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cb_assign_to_pending.Location = new System.Drawing.Point(129, 130);
+            this.cb_assign_to_pending.Name = "cb_assign_to_pending";
+            this.cb_assign_to_pending.Size = new System.Drawing.Size(106, 22);
+            this.cb_assign_to_pending.TabIndex = 7;
+            this.cb_assign_to_pending.TabStop = false;
+            this.cb_assign_to_pending.Tag = "ComponentName=Assign Article Count;";
+            this.cb_assign_to_pending.Text = "Assign To Pending";
+            this.cb_assign_to_pending.Visible = false;
+            this.cb_assign_to_pending.Click += new System.EventHandler(this.cb_assign_to_pending_clicked);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "WFullArticalCountForm";
+            // 
+            // WFullArticalCountForm
+            // 
+            this.ClientSize = new System.Drawing.Size(504, 163);
+            this.Controls.Add(this.cb_scaling_factor);
+            this.Controls.Add(this.cb_assign_to_pending);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WFullArticalCountForm";
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.cb_assign_to_pending, 0);
+            this.Controls.SetChildIndex(this.cb_scaling_factor, 0);
+            this.Controls.SetChildIndex(this.cb_scaling, 0);
+            this.Controls.SetChildIndex(this.cb_cancel, 0);
+            this.Controls.SetChildIndex(this.cb_ok, 0);
+            this.Controls.SetChildIndex(this.dw_artical_count, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         /// <summary>
@@ -174,15 +213,17 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
                 // dw_artical_count.GetItem<ContractArticalCountForm>(0).AcScaleFactor = 0;
                 // dw_Artical_Count.SetItem(1, "ac_scale_factor", dScale)
                 dw_artical_count.GetItem<ContractArticalCountForm>(0).AcScaleFactor = Convert.ToDecimal(dScale);
+            
                 if (ilRenewal > 0)
                 {
-                    cb_assign_to_pending.Enabled = true;
+                    cb_assign_to_pending.Enabled = false;   // true;
                 }
             }
             else
             {
                 cb_assign_to_pending.Enabled = false;
             }
+            cb_assign_to_pending.Visible = false;
         }
 
         public virtual void cb_assign_to_pending_clicked(object sender, EventArgs e)
@@ -198,7 +239,7 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
             // SELECT count(artical_count.contract_no)
             //  WHERE artical_count.contract_no = lContract
             //    AND artical_count.contract_seq_number = ilRenewal
-            lCount = RDSDataService.GetArticalCountCount2(lContract, ilRenewal);
+            lCount = RDSDataService.GetArticalCountRows(lContract, ilRenewal);
             if (lCount > 0)
             {
                 // TJB RPI_014 Oct-2010: Changed order of strings
@@ -263,8 +304,10 @@ namespace NZPostOffice.RDS.Windows.Ruralwin2
                 dw_artical_count.SetValue(0, "contract_seq_number", ilRenewal);
             }
             cb_assign_to_pending.Enabled = false;
+            cb_assign_to_pending.Visible = false;
         }
 
         #endregion
+
     }
 }
