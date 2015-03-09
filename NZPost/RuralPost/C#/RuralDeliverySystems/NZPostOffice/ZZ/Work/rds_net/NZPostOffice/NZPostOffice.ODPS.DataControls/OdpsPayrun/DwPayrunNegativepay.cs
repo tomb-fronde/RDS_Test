0bb2c42bc979293c7af3a652d06d93a188171d90
Brittont <brittont@fronde.com>
2015-03-09 23:12:54 +0000

    TJB  RPCR_094  Mar-2015 - NEW

---
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsPayrun/DwPayrunNegativepay.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Metex.Core;
+using NZPostOffice.ODPS.Entity.OdpsPayrun;
+
+namespace NZPostOffice.ODPS.DataControls.OdpsPayrun
+{
+    // TJB  RPCR_094  Mar-2015 - NEW
+    
+    public partial class DwPayrunNegativepay : Metex.Windows.DataUserControl
+    {
+        public DwPayrunNegativepay()
+        {
+            InitializeComponent();
+        }
+
+        public override int Retrieve()
+        {
+            return RetrieveCore<PayrunNegativepay>(new List<PayrunNegativepay>
+                (PayrunNegativepay.GetAllPayrunNegativepay()));
+        }
+    }
+}
