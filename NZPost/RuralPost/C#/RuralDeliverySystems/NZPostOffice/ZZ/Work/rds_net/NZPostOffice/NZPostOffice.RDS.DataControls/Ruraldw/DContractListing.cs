5d7ad1aa5d398bf6d2f8835f85f2566028cf0a5b
Brittont <brittont@fronde.com>
2018-07-26 01:52:58 +0000

    // TJB  RPCR_122  July-2018     // Added in_PbuId to retrieve parameter list

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DContractListing.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DContractListing.cs
@@ -10,6 +10,9 @@ using NZPostOffice.RDS.Entity.Ruraldw;
 
 namespace NZPostOffice.RDS.DataControls.Ruraldw
 {
+    // TJB  RPCR_122  July-2018
+    // Added in_PbuId to retrieve parameter list
+
 	public partial class DContractListing : Metex.Windows.DataUserControl
 	{
 		public DContractListing()
@@ -26,10 +29,15 @@ namespace NZPostOffice.RDS.DataControls.Ruraldw
             }
         }
 
-		public int Retrieve( int? in_Region, int? in_Contract, string in_ContractTitle, string in_ConMSN, DateTime? in_LastServiceStart, DateTime? in_LastServiceEnd, DateTime? in_LastDelStart, DateTime? in_LastDelEnd, DateTime? in_LastWorkStart, DateTime? in_LastWorkEnd, int? in_ContractType )
+		public int Retrieve( int? in_Region, int? in_Contract, string in_ContractTitle, string in_ConMSN
+                           , DateTime? in_LastServiceStart, DateTime? in_LastServiceEnd
+                           , DateTime? in_LastDelStart, DateTime? in_LastDelEnd
+                           , DateTime? in_LastWorkStart, DateTime? in_LastWorkEnd
+                           , int? in_ContractType, int? in_PbuId )
 		{
-			return RetrieveCore<ContractListing>(new List<ContractListing>
-				(ContractListing.GetAllContractListing( in_Region, in_Contract, in_ContractTitle, in_ConMSN, in_LastServiceStart, in_LastServiceEnd, in_LastDelStart, in_LastDelEnd, in_LastWorkStart, in_LastWorkEnd, in_ContractType )));
+			return RetrieveCore<ContractListing>(new List<ContractListing>(ContractListing.GetAllContractListing( in_Region, in_Contract, in_ContractTitle
+                                , in_ConMSN, in_LastServiceStart, in_LastServiceEnd, in_LastDelStart
+                                , in_LastDelEnd, in_LastWorkStart, in_LastWorkEnd, in_ContractType, in_PbuId)));
 		}
 
         public event EventHandler CellDoubleClick;
