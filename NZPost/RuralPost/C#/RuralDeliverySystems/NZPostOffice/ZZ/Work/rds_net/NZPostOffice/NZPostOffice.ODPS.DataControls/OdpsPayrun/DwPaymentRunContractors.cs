9c9bec753479c73d929215b6bd2bef570086b0d3
Brittont <brittont@fronde.com>
2019-06-11 02:24:21 +0000

    TJB  RPCR_140  June-2019     Added rg_code and contract_no to parameter lists

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsPayrun/DwPaymentRunContractors.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsPayrun/DwPaymentRunContractors.cs
@@ -10,17 +10,23 @@ using NZPostOffice.ODPS.Entity.OdpsPayrun;
 
 namespace NZPostOffice.ODPS.DataControls.OdpsPayrun
 {
+    // TJB  RPCR_140  June-2019
+    // Added rg_code and contract_no to parameter lists
+
     public partial class DwPaymentRunContractors : Metex.Windows.DataUserControl
     {
         public DwPaymentRunContractors()
         {
             InitializeComponent();
         }
-
-        public int Retrieve(string inOwnerDriver, DateTime? sdate, DateTime? edate)
+        // TJB  RPCR_140  June-2019
+        // Added rg_code and contract_no to parameter lists
+        public int Retrieve(string inOwnerDriver, DateTime? sdate, DateTime? edate
+                           , int? rg_code, int? contract_no)
         {
             return RetrieveCore<PaymentRunContractors>(new List<PaymentRunContractors>
-                (PaymentRunContractors.GetAllPaymentRunContractors(inOwnerDriver, sdate, edate)));
+                (PaymentRunContractors.GetAllPaymentRunContractors(inOwnerDriver, sdate, edate
+                                                                  , rg_code, contract_no)));
         }
     }
 }
