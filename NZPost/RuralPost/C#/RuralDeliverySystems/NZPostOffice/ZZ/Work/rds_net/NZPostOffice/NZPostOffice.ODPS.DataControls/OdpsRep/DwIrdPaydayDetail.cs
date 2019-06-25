212a3cc761999169145f7b89f3cb5fdaea4adb59
Brittont <brittont@fronde.com>
2019-06-25 04:14:04 +0000

    TJB  RPCR_128  June-2019: New     Derived from DwIr348Detail

---
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsRep/DwIrdPaydayDetail.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Metex.Core;
+using NZPostOffice.ODPS.Entity.OdpsRep;
+
+namespace NZPostOffice.ODPS.DataControls.OdpsRep
+{
+    // TJB  RPCR_128  June-2019: New
+    // Derived from DwIr348Detail
+
+    public partial class DwIrdPaydayDetail : Metex.Windows.DataUserControl
+    {
+        public DwIrdPaydayDetail()
+        {
+            InitializeComponent();
+        }
+
+        public int Retrieve(DateTime? startdate, DateTime? enddate)
+        {
+            //!Filter:  gross_earnings <> 0 
+            List<IrdPaydayDetail> SourceList = new List<IrdPaydayDetail>();
+            IrdPaydayDetail [] beforeFilter = IrdPaydayDetail.GetAllIrdPaydayDetail(startdate, enddate);
+
+            foreach (IrdPaydayDetail item in beforeFilter)
+            {
+                // tjb  RPI_004  June-2010
+                // Changed GrossEarnings from decimal? to string in definition
+                // Changed test to suit.
+
+                //if (item.GrossEarnings != 0)
+                if (item.GrossEarnings != null
+                    && item.GrossEarnings.Length > 1
+                    && item.GrossEarnings != "0")
+                {
+                    SourceList.Add(item);
+                }
+            }
+
+            return RetrieveCore<IrdPaydayDetail>(new List<IrdPaydayDetail>(SourceList));
+
+            
+        }
+    }
+}
