b27bf02afa952bb7c0ed4f61363d2ddf6cc0a016
Brittont <brittont@fronde.com>
2019-06-25 04:12:22 +0000

    TJB  RPCR_128  June-2019: New     Derived from DwIr348DetailException

---
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.ODPS.DataControls/OdpsRep/DwIrdPaydayDetailException.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Metex.Core;
+using NZPostOffice.ODPS.Entity.OdpsRep;
+
+namespace NZPostOffice.ODPS.DataControls.OdpsRep
+{
+    // TJB  RPCR_128  June-2019: New
+    // Derived from DwIr348DetailException
+
+    public partial class DwIrdPaydayDetailException : Metex.Windows.DataUserControl
+    {
+        public DwIrdPaydayDetailException()
+        {
+            InitializeComponent();
+        }
+
+        public int Retrieve(DateTime? startdate, DateTime? enddate)
+        {
+            return RetrieveCore<IrdPaydayDetailException>(new List<IrdPaydayDetailException>
+                (IrdPaydayDetailException.GetAllIrdPaydayDetailException(startdate, enddate)));
+        }
+    }
+}
