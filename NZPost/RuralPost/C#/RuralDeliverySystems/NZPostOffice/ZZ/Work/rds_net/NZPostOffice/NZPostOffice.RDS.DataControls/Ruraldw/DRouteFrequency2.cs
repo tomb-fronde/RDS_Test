58a72e5968071d92488481c788b9dab0562b7b77
Brittont <brittont@fronde.com>
2020-12-03 00:57:01 +0000

    TJB Frequencies 3-Dec-2020     Completed changes for Frequencies tab

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DRouteFrequency2.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DRouteFrequency2.cs
@@ -13,15 +13,30 @@ namespace NZPostOffice.RDS.DataControls.Ruraldw
     // TJB Frequencies Nov-2020
     // Fixed bug with clearBackColour
     //
-    // TJB Frequencies Changes 15-Nov-2020
-    // Checkin working version witb DRouteFrequency2Rows
-    //
     // TJB Frequencies Nov-2020
     // Created modelled on DContractFixedAssets
-    //
 
     public partial class DRouteFrequency2 : Metex.Windows.DataUserControl
 	{
+        // TJB  Frequencies Nov-2020 Notes (see also DRouteFrequency2Rows)
+        // The original frequencies tab was a single-row Metex Grid. To get two
+        // lines per row I used the technique used by DContractFixedAssets.
+        // During development, I kept the original functioning and called 
+        // the newer version Frequency2 (and related names).  Code for the original 
+        // has been removed from WContract2001 but should still be available 
+        // in the SourceSafe/GIT archive.
+        //
+        // The contract_no is included as a component, but is hidden.
+        // Its a way to have the value available on each frequency
+        // and is used to pass the contract_no to the vehicle_number 
+        // dropdown DDddwContractorVehicles.
+        //
+        // DDddwContractorVehicles finds all the vehicles owned/used by the contractor. 
+        // The contractor may hold other contracts; the vehicle list is made up of all 
+        // the distinct vehicles from all the contractor’s current contracts.
+        //
+        // Rows are added to the tab in DRouteFrequency2.bindingSource_ListChanged()
+
         public DRouteFrequency2()
 		{
 			InitializeComponent();
--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DRouteFrequency2Rows.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruraldw/DRouteFrequency2Rows.cs
@@ -11,19 +11,29 @@ using NZPostOffice.RDS.Entity.Ruraldw;
 namespace NZPostOffice.RDS.DataControls.Ruraldw
 {
     // TJB Frequencies Nov-2020
-    // Code tidied
-    //
-    // TJB Frequencies Changes 15-Nov-2020
-    // Checkin working version witb DRouteFrequency2
-    //
-    // TJB Frequencies Nov-2020
     // Created modelled on DContractFixedAssetsTest
+    // Displays the individual double-row entries in the Contract/Frequencies tab
 
     public partial class DRouteFrequency2Rows : Metex.Windows.DataUserControl
 	{
-        // TJB  RPCR_026  July-2011
-        // Added Load and Enter event handlers
-        // Added contract_no to screen
+        // TJB  Frequencies Nov-2020 Notes
+        // The original frequencies tab was a single-row Metex Grid. To get two
+        // lines per row I used the technique used by DContractFixedAssets.
+        // During development, I kept the original functioning and called 
+        // the newer version Frequency2 (and related names).  Code for the original 
+        // has been removed from WContract2001 but should still be available 
+        // in the SourceSafe/GIT archive.
+        //
+        // The contract_no is included as a component, but is hidden.
+        // Its a way to have the value available on each frequency
+        // and is used to pass the contract_no to the vehicle_number 
+        // dropdown DDddwContractorVehicles.
+        //
+        // DDddwContractorVehicles finds all the vehicles owned/used by the contractor. 
+        // The contractor may hold other contracts; the vehicle list is made up of all 
+        // the distinct vehicles from all the contractor’s current contracts.
+        //
+        // Rows are added to the tab in DRouteFrequency2.bindingSource_ListChanged()
 
         public DRouteFrequency2Rows()
         {
