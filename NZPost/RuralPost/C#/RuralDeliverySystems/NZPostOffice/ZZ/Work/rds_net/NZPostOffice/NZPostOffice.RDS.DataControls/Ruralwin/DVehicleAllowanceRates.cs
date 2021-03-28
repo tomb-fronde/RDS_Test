0be1c9587e181db3c4dcd3e46895d22ac21f4e5f
Brittont <brittont@fronde.com>
2021-03-28 02:42:46 +0000

    TJB  Allowances  28-Mar-2021:  New     Data store for vehicle allowance calculations

---
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.DataControls/Ruralwin/DVehicleAllowanceRates.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Text;
+using System.Windows.Forms;
+using Metex.Core;
+using NZPostOffice.RDS.Entity.Ruralwin;
+
+namespace NZPostOffice.RDS.DataControls.Ruralwin
+{
+    // TJB  Allowances  28-Mar-2021:  New
+    // Data store for vehicle allowance calculations
+
+    public partial class DVehicleAllowanceRates : Metex.Windows.DataUserControl
+    {
+        public DVehicleAllowanceRates()
+        {
+            InitializeComponent();
+        }
+
+        public int Retrieve(int? in_var_id)
+        {
+            return RetrieveCore<VehicleAllowanceRates>(VehicleAllowanceRates.GetAllVehicleAllowanceRates(in_var_id));
+        }
+
+        private void DVehicleAllowanceRates_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
