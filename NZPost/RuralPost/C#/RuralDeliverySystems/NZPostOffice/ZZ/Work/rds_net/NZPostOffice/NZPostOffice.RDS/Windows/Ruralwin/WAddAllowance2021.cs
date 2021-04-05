8778ed39d5519dcdda60d670c04d9b507ca30ce2
Brittont <brittont@fronde.com>
2021-04-05 03:42:29 +0000

    TJB  Allowances  4-Apr-2021: New     Adapted from WAddAllowance.     Vastly different: Only used to determine the new allowance's type     and associated vehicle if the calc type is distance-based.

---
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin/WAddAllowance2021.cs
@@ -0,0 +1,385 @@
+using System;
+using System.Windows.Forms;
+using NZPostOffice.RDS.Controls;
+using NZPostOffice.Shared;
+using NZPostOffice.RDS.Entity.Ruralwin;
+using NZPostOffice.RDS.DataService;
+using System.Collections.Generic;
+using NZPostOffice.RDS.DataControls.Ruralwin;
+using NZPostOffice.Shared.VisualComponents;
+using Metex.Windows;
+
+namespace NZPostOffice.RDS.Windows.Ruralwin
+{
+    // TJB  Allowances  4-Apr-2021: New
+    // Adapted from WAddAllowance.
+    // Vastly different: Only used to determine the new allowance's type 
+    // and associated vehicle if the calc type is distance-based.
+    //
+    // Once determined, the new record is saved and WMaintainAllowance is 
+    // initiated with the allowance calc type included so that WMaintainAllowance
+    // can immediately switch to the correct tab.
+
+    public class WAddAllowance2021 : WAncestorWindow
+    {
+        #region Define
+        
+        private object CallingObject;
+
+        public int il_contract;
+        public int il_contract_seq;
+        public int newRow = -1;
+
+        public int? il_altKey;
+        public int? il_alctId;
+        public int  il_caRow;
+        public string ls_title;
+
+        public string is_errmsg = String.Empty;
+        private System.ComponentModel.IContainer components = null;
+
+        public URdsDw dw_allowance;
+        public URdsDw idw_allowance;
+
+        public Button cb_save;
+        private Label Title;
+        public Button cb_cancel;
+
+        #endregion
+
+        public WAddAllowance2021()
+        {
+            this.InitializeComponent();
+
+            this.dw_allowance.DataObject = new DAddAllowance();
+            dw_allowance.DataObject.BorderStyle = BorderStyle.None;
+            idw_allowance = dw_allowance;
+        }
+
+        #region FormDesign
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dw_allowance = new NZPostOffice.RDS.Controls.URdsDw();
+            this.cb_save = new System.Windows.Forms.Button();
+            this.cb_cancel = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // st_label
+            // 
+            this.st_label.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.st_label.Location = new System.Drawing.Point(8, 183);
+            this.st_label.Size = new System.Drawing.Size(136, 23);
+            this.st_label.Text = "WAddAllowance2021";
+            // 
+            // dw_allowance
+            // 
+            this.dw_allowance.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dw_allowance.DataObject = null;
+            this.dw_allowance.FireConstructor = false;
+            this.dw_allowance.Location = new System.Drawing.Point(12, 32);
+            this.dw_allowance.Name = "dw_allowance";
+            this.dw_allowance.Size = new System.Drawing.Size(576, 123);
+            this.dw_allowance.TabIndex = 1;
+            // 
+            // cb_save
+            // 
+            this.cb_save.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.cb_save.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cb_save.Location = new System.Drawing.Point(427, 172);
+            this.cb_save.Name = "cb_save";
+            this.cb_save.Size = new System.Drawing.Size(75, 23);
+            this.cb_save.TabIndex = 2;
+            this.cb_save.Text = "Save";
+            this.cb_save.Click += new System.EventHandler(this.cb_save_clicked);
+            // 
+            // cb_cancel
+            // 
+            this.cb_cancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.cb_cancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.cb_cancel.Location = new System.Drawing.Point(513, 172);
+            this.cb_cancel.Name = "cb_cancel";
+            this.cb_cancel.Size = new System.Drawing.Size(75, 23);
+            this.cb_cancel.TabIndex = 3;
+            this.cb_cancel.Text = "Cancel";
+            this.cb_cancel.Click += new System.EventHandler(this.cb_cancel_clicked);
+            // 
+            // Title
+            // 
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(8, 9);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(217, 15);
+            this.Title.TabIndex = 4;
+            this.Title.Text = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+            // 
+            // WAddAllowance2021
+            // 
+            this.AcceptButton = this.cb_save;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(599, 204);
+            this.Controls.Add(this.dw_allowance);
+            this.Controls.Add(this.cb_save);
+            this.Controls.Add(this.Title);
+            this.Controls.Add(this.cb_cancel);
+            this.Location = new System.Drawing.Point(46, 55);
+            this.Name = "WAddAllowance2021";
+            this.Text = "Select Allowance Type";
+            this.Controls.SetChildIndex(this.cb_cancel, 0);
+            this.Controls.SetChildIndex(this.Title, 0);
+            this.Controls.SetChildIndex(this.cb_save, 0);
+            this.Controls.SetChildIndex(this.dw_allowance, 0);
+            this.Controls.SetChildIndex(this.st_label, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        public virtual void dw_allowance_constructor()
+        {
+            idw_allowance = dw_allowance;
+        }
+
+        public override void pfc_preopen()
+        {
+            base.pfc_preopen();
+        }
+
+        public override void pfc_postopen()
+        {
+            int nRow, nRows;
+            NRdsMsg lnv_msg;
+            NCriteria lvn_Criteria;
+
+            DateTime dtEffDate = DateTime.MinValue;
+            DateTime dtToday = DateTime.Today.Date;
+            DateTime? dtPaidToDate, tmpDate;
+
+            base.pfc_postopen();
+            this.of_set_componentname("Allowance");
+
+            lnv_msg = (NRdsMsg)StaticMessage.PowerObjectParm;
+            lvn_Criteria = lnv_msg.of_getcriteria();
+            il_contract = System.Convert.ToInt32(lvn_Criteria.of_getcriteria("contract_no"));
+            il_contract_seq = System.Convert.ToInt32(lvn_Criteria.of_getcriteria("con_active_seq"));
+            il_altKey = (int?)System.Convert.ToInt32(lvn_Criteria.of_getcriteria("alt_key"));
+            il_caRow = System.Convert.ToInt32(lvn_Criteria.of_getcriteria("allowance_row"));
+            ls_title = lvn_Criteria.of_getcriteria("contract_title") as string;
+
+            CallingObject = StaticVariables.window;
+            this.Title.Text = ls_title;
+
+            // Get the Effective Date for this contract.
+            // We only list those allowances whose effective date falls in the Contract's current period
+            tmpDate = RDSDataService.GetConRatesEffDate((int?) il_contract, (int?) il_contract_seq);
+            dtEffDate = (DateTime)tmpDate;
+
+            idw_allowance.DataObject.Reset();
+            //nRows = ((DAddAllowance)idw_allowance.DataObject).Retrieve(il_contract, dtEffDate);
+            //((DAddAllowance)idw_allowance.DataObject).Retrieve(il_contract, dtToday);
+
+            // Insert a new record at the beginning of the list
+            newRow = 0;
+            idw_allowance.DataObject.InsertItem<AddAllowance>(newRow, new AddAllowance());
+            nRows = idw_allowance.RowCount;
+            int n = nRows;
+            // NOTE: 'newRow' is the row number of the added row.  If the row is added 
+            // elsewhere (eg at the end of the list), newRow must be changed.
+            // Set default values on the new row
+            idw_allowance.GetItem<AddAllowance>(newRow).ContractTitle = ls_title;
+            idw_allowance.GetItem<AddAllowance>(newRow).ContractNo = il_contract;
+            idw_allowance.GetItem<AddAllowance>(newRow).EffectiveDate = dtToday;
+            idw_allowance.GetItem<AddAllowance>(newRow).Approved = "N";
+
+            // Set the focus on the new record (which has been added (usually as the first row))
+            ((DAddAllowance)(idw_allowance.DataObject)).SetCurrent(newRow);
+            
+            idw_allowance.DataObject.BindingSource.CurrencyManager.Refresh();
+        }
+
+        public void of_open_WMaintainAllowance()
+        {
+            NRdsMsg lnv_msg;
+            NCriteria lvn_Criteria;
+
+            lnv_msg = (NRdsMsg)StaticMessage.PowerObjectParm;
+            lvn_Criteria = lnv_msg.of_getcriteria();
+
+            il_altKey = idw_allowance.GetItem<AddAllowance>(0).AltKey;
+            il_alctId = idw_allowance.GetItem<AddAllowance>(0).AlctId;
+
+            Cursor.Current = Cursors.WaitCursor;
+            lvn_Criteria.of_addcriteria("contract_no", il_contract);
+            lvn_Criteria.of_addcriteria("con_active_seq", il_contract_seq);
+            lvn_Criteria.of_addcriteria("alt_key", il_altKey);
+            lvn_Criteria.of_addcriteria("alct_id", il_alctId);
+            lvn_Criteria.of_addcriteria("contract_title", ls_title);
+            lvn_Criteria.of_addcriteria("optype", "Update");
+            lnv_msg.of_addcriteria(lvn_Criteria);
+
+            // TJB  RPCR_017 July-2010
+            // Changed WAddAllowance significantly
+            // (See WAddAllowance0 for previous version)
+            StaticMessage.PowerObjectParm = lnv_msg;
+            StaticVariables.window = CallingObject;  // WContract2001
+
+            WMaintainAllowances w_maintain_allowances = new WMaintainAllowances();
+
+            w_maintain_allowances.MdiParent = StaticVariables.MainMDI;
+            w_maintain_allowances.Show();
+            return;
+        }
+
+        public override void close()
+        {
+/*
+            //  Tell the parent to refresh
+            ((WContract2001)StaticVariables.window).idw_allowances.Reset();
+            ((WContract2001)StaticVariables.window).idw_allowances.Retrieve(new object[]{il_contract});
+            StaticVariables.window = null;
+*/
+        }
+
+        public virtual int wf_validate(int pRow, out string pErrmsg)
+        {
+            // TJB 18-Mar-2021 Added sAltDescription
+
+            // Validate the record at arow.
+            //
+            // Return codes
+            //   SUCCESS  - Record is OK
+            //   FAILURE  - Something's wrong.  Pass error message back in errmsg.
+            //
+
+            int? ll_altkey, ll_varid, ll_alctid;
+            DateTime? ld_maxdate, ld_effdate;
+
+            pErrmsg = "Succeeded";
+
+            // Check to see if anything has been entered for the inserted record
+            // If not, accept the record; it will not be inserted (silently).
+            ll_altkey = idw_allowance.GetItem<AddAllowance>(pRow).AltKey;
+            if (ll_altkey == null)
+            {
+                pErrmsg = "You must enter an allowance type.";
+                ((DAddAllowance)(idw_allowance.DataObject)).SetGridCellSelected(pRow, "alt_key", true);
+                return FAILURE;
+            }
+            ld_effdate = idw_allowance.GetItem<AddAllowance>(pRow).EffectiveDate;
+            if (ld_effdate == null || ld_effdate == DateTime.MinValue)
+            {
+                pErrmsg = "You must enter an effective date.";
+                ((DAddAllowance)(idw_allowance.DataObject)).SetGridCellSelected(pRow, "ca_effective_date", true);
+                return FAILURE;
+            }
+            ll_alctid = idw_allowance.GetItem<AddAllowance>(newRow).AlctId;
+            ll_alctid = (ll_alctid == null) ? 0 : ll_alctid;
+            ll_varid = idw_allowance.GetItem<AddAllowance>(pRow).VarId;
+            if (ll_varid == null && ll_alctid == 5)
+            {
+                pErrmsg = "You must enter a vehicle type.";
+                ((DAddAllowance)(idw_allowance.DataObject)).SetGridCellSelected(pRow, "var_id", true);
+                return FAILURE;
+            }
+
+            // if this is the new record, check that its effective date is later than that
+            // of any existing allowances of the same type.
+            if (newRow >= 0 && pRow == newRow)
+            {
+                ld_maxdate = new DateTime();
+                int SQLCode = 0;
+                string SQLErrText = string.Empty;
+                ld_maxdate = RDSDataService.GetContractAllownceMaxCaEffective(il_contract, ll_altkey, ref SQLCode, ref SQLErrText);
+
+                if (SQLCode != 0 && SQLCode != 100)
+                {
+                    MessageBox.Show("Failed looking up max(ca_effective_date): \n"
+                                    + "contract=" + il_contract.ToString()
+                                    + ", alt_key=" + ll_altkey.ToString() + "\n\n"
+                                    + "Error Code: " + SQLCode.ToString() + "\n"
+                                    + "Error Text: " + SQLErrText
+                                    , "Database error");
+                    pErrmsg = "Failed looking up max(ca_effective_date)";
+                    ((DAddAllowance)(idw_allowance.DataObject)).SetGridCellSelected(pRow, "ca_effective_date", true);
+                    return FAILURE;
+                }
+
+                if (ld_effdate <= ld_maxdate)
+                {
+                    pErrmsg = "The effective date must be greater than "
+                                + ld_maxdate.GetValueOrDefault().ToString("dd/MM/yyyy") + '.';
+                    return FAILURE;
+                }
+            }
+            return SUCCESS;
+        }
+
+        #region Events
+        public virtual void cb_save_clicked(object sender, EventArgs e)
+        {
+            cb_save.Focus();
+            int rc, nRow, nRows;
+            string errmsg = "";
+
+            idw_allowance.DataObject.AcceptText();
+            is_errmsg = "";
+
+            nRows = idw_allowance.RowCount;
+            for (nRow = 0; nRow < nRows; nRow++)
+            {
+                // Only validate the row if it has been changed; assume an unchanged row is OK
+                // The deeper reason is that within the validate function, the effective 
+                // date is checked against the current date (actually a date 90 days in 
+                // the past) and it is possible the effective date on an unchanged record 
+                // will fail this test.
+                bool isRowChanged = StaticFunctions.IsDirty(idw_allowance.DataObject, nRow);
+                if (! isRowChanged) continue;
+                rc = wf_validate(nRow, out errmsg);
+                if (!(rc == SUCCESS))
+                {
+                    ((DAddAllowance)(idw_allowance.DataObject)).SetCurrent(nRow);
+                    this.ResumeLayout(true);
+                    if (rc == FAILURE)
+                        MessageBox.Show(errmsg, "Error"
+                                       , MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    return;
+                }
+            }
+
+            of_open_WMaintainAllowance();
+
+            idw_allowance.DataObject.Save();
+            this.Close();
+        }
+
+        public virtual void cb_cancel_clicked(object sender, EventArgs e)
+        {
+            idw_allowance.DataObject.Reset();
+            this.Close();
+        }
+        #endregion
+    }
+}
