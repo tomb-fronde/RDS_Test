621796fbf42d638bd318fdbaf0c00216728ee802
Brittont <brittont@fronde.com>
2021-06-07 23:49:02 +0000

    Vss2Git

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin/WMainMdi.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS/Windows/Ruralwin/WMainMdi.cs
@@ -39,13 +39,13 @@ namespace NZPostOffice.RDS.Windows.Ruralwin
             this.InitializeComponent();
             m_main_menu = new MMainMenu(this);
             this.Text = "Rural Delivery System";
-            this.toolStripStatusLabel4.Text = "Production Version";
+            //this.toolStripStatusLabel4.Text = "Production Version";
             //this.toolStripStatusLabel4.Text = "DEV Version";
-            //this.toolStripStatusLabel4.Text = "Test Version";
+            this.toolStripStatusLabel4.Text = "Test Version";
             // since its value was meaningless (just like the clock).
             //this.oolStripStatusLabel3.Text = "Environment 8.0.00";
-            this.toolStripStatusLabel3.Text = "RDS 7.1.17.2";
-            this.toolStripStatusLabel6.Text = "20-June-2016 9:30";
+            this.toolStripStatusLabel3.Text = "RDS 7.1.17.5";
+            this.toolStripStatusLabel6.Text = "8-June-2021 ";
             this.toolStripStatusLabel6.Text = string.Format("{0:dd-MMM-yyyy HH:mm}",
                        System.IO.File.GetLastWriteTime(System.Reflection.Assembly.GetEntryAssembly().Location));
         }
