194da20c2bd4380781d84e6be81ce9f47289d643
Brittont <brittont@fronde.com>
2021-07-08 22:58:08 +0000

    [5-Jul-2021] Replaced Fetch SQL query with stored procedure

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.Entity/ruralwin/MaintainAllowanceV2.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPostOffice.RDS.Entity/ruralwin/MaintainAllowanceV2.cs
@@ -13,6 +13,7 @@ namespace NZPostOffice.RDS.Entity.Ruralwin
     // Merged MaintainAllowances and MaintainVehAllowances to rationalise WMaintainAllowances code
     // [22-June-2021] Changed how function f_GetAllowanceAmount.sql determines net_amount
     //                for fixed-type calculations (no change to MaintainAllowancesV2)
+    // [5-Jul-2021] Replaced Fetch SQL query with stored procedure
     //
     // TJB Allowances 29-Mar-2021: New
     // Get details for the DMaintainDistanceAllowances tab
@@ -1008,57 +1009,57 @@ namespace NZPostOffice.RDS.Entity.Ruralwin
                     _sqlerrtext = "";
                     _sqlcode = 0;
                     ParameterCollection pList = new ParameterCollection();
-                    cm.CommandType = CommandType.Text;
-					//GenerateSelectCommandText(cm, "contract_allowance");
-					//GenerateSelectCommandText(cm, "contract");
-                    cm.CommandText =
-                            "SELECT ca.alt_key "
-                                + ", ca.contract_no "
-                                + ", ca.ca_effective_date "
-                                + ", ca.ca_annual_amount "
-                                + ", ca.ca_notes "
-                                + ", ca.ca_paid_to_date "
-                                + ", ca.ca_approved "
-                                + ", c.con_title "
-                                + ", ca.ca_var1 "
-                                + ", ca.ca_dist_day "
-                                + ", ca.ca_hrs_wk "
-                                + ", alt.alt_description "
-                                + ", alt.alt_rate "
-                                + ", alt.alt_wks_yr "
-                                + ", alt.alt_acc "
-                                + ", alct.alct_id "
-                                + ", alct.alct_description "
-                                + ", ca.ca_doc_description "
-                                + ", ca.ca_costs_covered "
-                                + ", var.var_id "
-                                + ", var.var_description "
-                                + ", var.var_carrier_pa "
-                                + ", var.var_repairs_pk "
-                                + ", var.var_licence_pa "
-                                + ", var.var_tyres_pk "
-                                + ", var.var_allowance_pk "
-                                + ", var.var_insurance_pa "
-                                + ", var.var_ror_pa "
-                                + ", var.var_fuel_use_pk "
-                                + ", var.var_fuel_rate "
-                                + ", var.var_ruc_rate_pk "
-                                + ", ca.ca_row_changed "
-                                + ", rd.f_GetAllowanceAmount(ca.contract_no, ca.alt_key, ca_effective_date) "
-                            + " FROM rd.contract_allowance ca "
-                                + ", rd.contract c "
-                                + ", rd.allowance_type alt "
-                                + ", rd.allowance_calc_type alct "
-                                + ", rd.vehicle_allowance_rates var "
-                           + " WHERE ca.contract_no = @inContractNo "
-                           + "  AND c.contract_no = ca.contract_no "
-                           + "  AND alt.alt_key = ca.alt_key "
-                           + "  AND alt.alct_id =  @inAlctId "
-                           + "  AND alct.alct_id = alt.alct_id "
-                           + "  AND (var.var_id = ca.var_id " 
-                           + "       or (ca.var_id is null and var.var_id = 0))"
-                           + " ORDER BY alt.alt_description ASC "
-                                   + ", ca.ca_effective_date DESC ";
+                    cm.CommandType = CommandType.StoredProcedure;
+                    cm.CommandText = "rd.sp_getContractAllowanceDetails";
+                    //cm.CommandType = CommandType.Text;
+                    //cm.CommandText =
+                    //        "SELECT ca.alt_key "
+                    //            + ", ca.contract_no "
+                    //            + ", ca.ca_effective_date "
+                    //            + ", ca.ca_annual_amount "
+                    //            + ", ca.ca_notes "
+                    //            + ", ca.ca_paid_to_date "
+                    //            + ", ca.ca_approved "
+                    //            + ", c.con_title "
+                    //            + ", ca.ca_var1 "
+                    //            + ", ca.ca_dist_day "
+                    //            + ", ca.ca_hrs_wk "
+                    //            + ", alt.alt_description "
+                    //            + ", alt.alt_rate "
+                    //            + ", alt.alt_wks_yr "
+                    //            + ", alt.alt_acc "
+                    //            + ", alct.alct_id "
+                    //            + ", alct.alct_description "
+                    //            + ", ca.ca_doc_description "
+                    //            + ", ca.ca_costs_covered "
+                    //            + ", var.var_id "
+                    //            + ", var.var_description "
+                    //            + ", var.var_carrier_pa "
+                    //            + ", var.var_repairs_pk "
+                    //            + ", var.var_licence_pa "
+                    //            + ", var.var_tyres_pk "
+                    //            + ", var.var_allowance_pk "
+                    //            + ", var.var_insurance_pa "
+                    //            + ", var.var_ror_pa "
+                    //            + ", var.var_fuel_use_pk "
+                    //            + ", var.var_fuel_rate "
+                    //            + ", var.var_ruc_rate_pk "
+                    //            + ", ca.ca_row_changed "
+                    //            + ", rd.f_GetAllowanceAmount(ca.contract_no, ca.alt_key, ca_effective_date) "
+                    //        + " FROM rd.contract_allowance ca "
+                    //            + ", rd.contract c "
+                    //            + ", rd.allowance_type alt "
+                    //            + ", rd.allowance_calc_type alct "
+                    //            + ", rd.vehicle_allowance_rates var "
+                    //       + " WHERE ca.contract_no = @inContractNo "
+                    //       + "  AND c.contract_no = ca.contract_no "
+                    //       + "  AND alt.alt_key = ca.alt_key "
+                    //       + "  AND alt.alct_id =  @inAlctId "
+                    //       + "  AND alct.alct_id = alt.alct_id "
+                    //       + "  AND (var.var_id = ca.var_id " 
+                    //       + "       or (ca.var_id is null and var.var_id = 0))"
+                    //       + " ORDER BY alt.alt_description ASC "
+                    //               + ", ca.ca_effective_date DESC ";
 
                     pList.Add(cm, "inContractNo", inContractNo);
                     pList.Add(cm, "inAlctId", inAlctId);
