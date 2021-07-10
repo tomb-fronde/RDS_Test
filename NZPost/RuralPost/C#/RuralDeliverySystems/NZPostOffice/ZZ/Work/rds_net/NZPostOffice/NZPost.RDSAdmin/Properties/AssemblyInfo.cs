694ef19fa4c9ba83cc845b458129f23f962d1174
Brittont <brittont@fronde.com>
2021-07-10 00:44:01 +0000

    Vss2Git

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPost.RDSAdmin/Properties/AssemblyInfo.cs
+++ NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/NZPostOffice/NZPost.RDSAdmin/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("7.1.17.5")]
-[assembly: AssemblyFileVersion("7.1.17.5")]
+[assembly: AssemblyVersion("7.1.17.7")]
+[assembly: AssemblyFileVersion("7.1.17.7")]
