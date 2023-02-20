6217d5b3f00d31c36d649c66e4150aed1512102a
tomb-fronde <tom.britton@fronde.com>
2023-02-21 11:08:45 +1300

    Remove DDL, ALL_SP_FUNCTN_MSSQL.sq from cache

--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/DDL/ForeignKeys/rd/contract_renewals.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE [rd].[contract_renewals] WITH CHECK 
-  ADD CONSTRAINT [FK_contract2] 
-  FOREIGN KEY([contract_no])
-    REFERENCES [rd].[contract] ([contract_no])
-GO
-ALTER TABLE [rd].[contract_renewals] CHECK CONSTRAINT [FK_contract2]
-GO
--- NZPost/RuralPost/C#/RuralDeliverySystems/NZPostOffice/ZZ/Work/rds_net/DDL/ForeignKeys/rd/rate_days.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE [rd].[rate_days] WITH CHECK 
-  ADD CONSTRAINT [FK_standard_frequency] 
-  FOREIGN KEY([sf_key])
-    REFERENCES [rd].[standard_frequency] ([sf_key])
-GO
-ALTER TABLE [rd].[rate_days] CHECK CONSTRAINT [FK_standard_frequency]
-GO
